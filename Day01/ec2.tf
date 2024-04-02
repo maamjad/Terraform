f2c0ce843f06f1df81f8eca16ec078e2b218e7e9
Muhammad Amjad <144642317+maamjad@users.noreply.github.com>
2024-04-02 23:23:20 +0500

    ec2.tf

--- Day01/ec2.tf
+++ Day01/ec2.tf
@@ -1,14 +1,18 @@
+############# Terraform Provider Reference ##############
+
 provider "aws" {
   region     = "us-west-2"
   access_key = "my-access-key"
   secret_key = "my-secret-key"
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+############# How to create an ec2 Instance by using terraform ##############
+
+resource "aws_instance" "web-server" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Web-Server"
   }
 }
