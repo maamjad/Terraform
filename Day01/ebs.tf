b95c4bae4abb4b08ab33db177b352f987f6d6d01
Muhammad Amjad <144642317+maamjad@users.noreply.github.com>
2024-04-02 23:24:36 +0500

    ebs.tf

---
+++ Day01/ebs.tf
@@ -0,0 +1,9 @@
+############# Terraform Provider Reference ##############
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "key"
+  secret_key = "key"
+}
+
+############# How to create an ec2 Instance by using terraform ##############
